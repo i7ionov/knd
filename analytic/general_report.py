8dca9da35a4130664dbfc071733c21f2fdd00de8
ivsemionov <ivsemionov@iggn.permkrai.ru>
2019-02-14 15:42:07 +0500

    Убрал из ежемесячного отчета тестовые проверки

--- analytic/general_report.py
+++ analytic/general_report.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 from .models import GeneralReport, ViolationInGeneralReport, AbstractItemCountInReport
 from inspections.models import Inspection
+from django.db.models import Q
 
 
 def generate_general_report_period(user_id, date_begin, date_end, control_kind_id=None, department_id=None):
     report = GeneralReport(report_status='Формируется...', user_id=user_id, date=datetime.now())
     inspections = Inspection.objects.filter(doc_date__range=(date_begin, date_end))
+    # исключаем тестовые проверки
+    inspections = inspections.exclude(inspection_result_id=12)
     if control_kind_id:
-        inspections.filter(control_kind_id=control_kind_id)
+        inspections = inspections.filter(control_kind_id=control_kind_id)
         report.control_kind_id = control_kind_id
     if department_id:
-        inspections.filter(inspector__department_id=department_id)
+        inspections = inspections.filter(inspector__department_id=department_id)
         report.department_id = department_id
     iterate_inspections(inspections, report)
     return report
