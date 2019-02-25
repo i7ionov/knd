3bdd625a7714c9ee6547f5af6fe79b064768b375
ivsemionov <ivsemionov@iggn.permkrai.ru>
2019-02-25 09:13:00 +0500

    Теперь у проверки исполнения предписания инспектором будет текущий пользователь

--- inspections/views.py
+++ inspections/views.py
@@ -67,10 +67,7 @@ def new_inspection_form(request, control_kind=None, id=None):
             insp.control_form = precept.parent.inspection.control_form
             insp.control_plan = precept.parent.inspection.control_plan
             insp.organization = precept.organization
-            if precept.parent.inspection.inspector:
-                insp.inspector = precept.parent.inspection.inspector
-            else:
-                insp.inspector = User.objects.get(django_user=request.user)
+            insp.inspector = User.objects.get(django_user=request.user)
             insp.save()
             insp.houses.set(precept.houses.all())
         except (KeyError, models.Precept.DoesNotExist):
