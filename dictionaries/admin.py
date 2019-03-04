e5b796d04dfea663a4af0222912c99d93828a92c
ivsemionov <ivsemionov@iggn.permkrai.ru>
2019-03-04 15:53:47 +0500

    Добавил Отдел в админку

--- dictionaries/admin.py
+++ dictionaries/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+admin.site.register(models.Department)
 admin.site.register(models.User)
 admin.site.register(models.Article)
 admin.site.register(models.OrganizationType)
