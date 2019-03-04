23c7cd0102447b251943afab3d56aa7b76956e82
ivsemionov <ivsemionov@iggn.permkrai.ru>
2019-03-04 15:48:56 +0500

    Откорректировано отображение объекта Отдел в виде строки

--- dictionaries/models.py
+++ dictionaries/models.py
@@ -11,6 +11,9 @@ class Department(models.Model):
     name = models.CharField(max_length=200, verbose_name='отдел')
     name_rp = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = "отдел"
 
